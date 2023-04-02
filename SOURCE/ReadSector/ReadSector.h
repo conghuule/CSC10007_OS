f923300e6d1fbb1e628e42112f2797faab073906
Phlinh29102 <trlinh291@gmail.com>
2023-04-02 12:45:04 +0700

    Read and display sector

---
+++ SOURCE/ReadSector/ReadSector.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <windows.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+using namespace std;
+
+int ReadSector(LPCWSTR, int, BYTE sector[512]);
+unsigned int ByteToDec(BYTE n);
+string DecToHex(unsigned int n);
+unsigned int HexToDec(string n);
+void displaySector(BYTE sector[512]);
+int readByteByOffset(BYTE sector[512], string offset, unsigned int n_of_bytes);
