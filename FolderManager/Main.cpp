b5976423991925e2210669902dcb832d2b75fe8e
conghuule <you@example.com>
2023-04-04 22:26:32 +0700

    merge Read FAT32 & NTFS

---
+++ FolderManager/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include "FAT32.h"
+#include "NTFS.h"
+#include "ReadSector.h"
+
+int main(int argc, char** argv)
+{
+    BYTE sector[512];
+    string USB = "E";
+    wstring convert(USB.begin(), USB.end());
+    wstring path = L"\\\\.\\" + convert + L":";
+    ReadSector(path.c_str(), 0, sector); //read the first sector, save info into sector array
+    //string res = "";
+    //cout << readByteByOffset(sector, res, "0b", 2); //read 2 BYTES from offset "0b"
+    displaySector(sector);
+    DisplayFAT32PBSInfo(sector);
+    DisplayPBSInfo(sector);
+    return 0;
+}
